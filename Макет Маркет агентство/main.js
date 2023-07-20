3330a6cf10039a4ea37a04b9bf19160bb88a18c5
BattalovaLinara <136588530+BattalovaLinara@users.noreply.github.com>
2023-07-20 17:54:14 +0300

    Add files via upload

---
+++ Макет Маркет агентство/main.js
@@ -0,0 +1,14 @@
+//let menuBtn = document.querySelector(".menu-btn");
+//let menu = document.querySelector(".nav");
+
+//menuBtn.addEventListener("click", function() {
+   // menu.classList.toggle("active");
+//})
+
+
+let burgerBtn = document.querySelector(".menu-btn");
+let menu = document.querySelector(".nav");
+
+burgerBtn.addEventListener("click", function() {
+    menu.classList.toggle("_active")
+})
